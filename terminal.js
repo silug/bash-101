dcb9ac7ebb6dac4faa8c620f99273807fe88c9b2
Steven Pritchard <steven.pritchard@onyxpoint.com>
2019-08-22 17:11:55 -0500

    Drop hard-coded URL to wetty.

--- terminal.js
+++ terminal.js
@@ -1,6 +1,6 @@
 $(document).ready(function () {
 	//add the new tab terminal button.
-	$("#closeMenu").before('<div id="terminaltoggle" style="visibility:visible;" class="buttonWrapper"><i class="fa fa-clone"></i><a href="https://training.onyxpoint.com/wetty" target="_blank">New Tab Terminal</a></div><hr>');
+	$("#closeMenu").before('<div id="terminaltoggle" style="visibility:visible;" class="buttonWrapper"><i class="fa fa-clone"></i><a href="/wetty" target="_blank">New Tab Terminal</a></div><hr>');
 
 	// Sets the terminal toggle option in the menu for the students
 	$("#fileDownloads").before('<div id="terminaltoggle" style="visibility:visible;" class="buttonWrapper"><i class="fa fa-gamepad"></i> Toggle Terminal</div><hr>');
@@ -9,7 +9,7 @@ $(document).ready(function () {
 	$("#closeMenu").before('<div id="puppetdocs" style="visibility:visible;" class="buttonWrapper"><i class="fa fa-book"></i><a href="https://docs.puppet.com/puppet/" target="_blank">Puppet Docs</a></div><hr>');
 
 	// injects the iframe for the terminal window to nest in the background
-	$("#preso").before('<iframe id="terminalwindow" style="width:0%; height:0%;" src="https://training.onyxpoint.com/wetty">Outside Webpage</iframe>');
+	$("#preso").before('<iframe id="terminalwindow" style="width:0%; height:0%;" src="/wetty">Outside Webpage</iframe>');
 
 	// This is activated when the class for the terminal window is clicked
 	$("#terminaltoggle").click(function () {
